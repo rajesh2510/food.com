aa6799a2f0add6b647aa8b1d31fe611a47300802
rajesh2510 <129522157+rajesh2510@users.noreply.github.com>
2024-08-19 23:58:18 +0530

    Update navbar.jsx

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -26,7 +26,7 @@ const navbar = ({setLogin}) => {
       <a href="#footer" onClick={() =>setMenu("contact us")} className={menu==="contact us"?"active":""}>contact us</a>
     </ul>
     <div className="navbar-right">
-      <img src={assets.search_icon} alt="" />
+      <img className="search" src={assets.search_icon} alt="" />
       <div className="navbar-basket">
        <Link to="/cart" ><img src={assets.basket_icon} alt="" /></Link> 
         <div className={getTotalCartAmount()===0?"":"dot"}></div>
