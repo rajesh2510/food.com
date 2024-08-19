ed5cc52d860ad8531c70b35824ab866a127ed021
rajesh2510 <129522157+rajesh2510@users.noreply.github.com>
2024-08-20 02:28:48 +0530

    Update Header.jsx

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
       <div className="header-contents">
        <h2>Order your favourite food here</h2>
        <p>Choose from a diverse menu featuring a delectable array of dishes crafted with love.</p>
-       <button>View Menu</button>
+       <button><a href="#explore-menu" >View Menu</a></button>
        </div>
        </div>
   )
